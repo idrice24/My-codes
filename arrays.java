7d333b8fab9ea4f5bfbfa54c0a8555afd652a368
idrice24 <idrice.tsafouet@yahoo.com>
2020-04-05 13:46:19 +0100

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hnd;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class arrays {
+    public void arrays(String [] args){
+    System.out.println("");
+    }
+}
